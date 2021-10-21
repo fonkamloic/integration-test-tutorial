c00eb77d0700f1807454dd9fc8ab2fce4581a935
Fonkam Loïc <fonkamloic@gmail.com>
2021-10-21 11:00:19 -0400

    Finished project

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -7,27 +7,26 @@ void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets(
-    "Not inputting a text and wanting to go to the display page shows "
-    "an error and prevents from going to the display page.",
-    (WidgetTester tester) async {
-      await tester.pumpWidget(MyApp());
+      "Not inputtinga text and wanting to go to the display page shows"
+      "an error and prevent from going to the display page.",
+      (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
 
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
 
-      expect(find.byType(TypingPage), findsOneWidget);
-      expect(find.byType(DisplayPage), findsNothing);
-      expect(find.text('Input at least one character'), findsOneWidget);
-    },
-  );
+    expect(find.byType(TypingPage), findsOneWidget);
+    expect(find.byType(DisplayPage), findsNothing);
+    expect(find.text('Input at least one character'), findsOneWidget);
+  });
 
   testWidgets(
-    "After inputting a text, go to the display page which contains that same text "
+    "After inputting a text, go to the display page when contains that same text"
     "and then navigate back to the typing page where the input should be clear",
     (WidgetTester tester) async {
       await tester.pumpWidget(MyApp());
 
-      final inputText = 'Hello there, this is an input.';
+      final inputText = 'Hello there, this in an input.';
       await tester.enterText(find.byKey(Key('your-text-field')), inputText);
 
       await tester.tap(find.byType(FloatingActionButton));
@@ -45,4 +44,44 @@ void main() {
       expect(find.text(inputText), findsNothing);
     },
   );
+
+  // testWidgets(
+  //   "Not inputting a text and wanting to go to the display page shows "
+  //   "an error and prevents from going to the display page.",
+  //   (WidgetTester tester) async {
+  //     await tester.pumpWidget(MyApp());
+
+  //     await tester.tap(find.byType(FloatingActionButton));
+  //     await tester.pumpAndSettle();
+
+  //     expect(find.byType(TypingPage), findsOneWidget);
+  //     expect(find.byType(DisplayPage), findsNothing);
+  //     expect(find.text('Input at least one character'), findsOneWidget);
+  //   },
+  // );
+
+  // testWidgets(
+  //   "After inputting a text, go to the display page which contains that same text "
+  //   "and then navigate back to the typing page where the input should be clear",
+  //   (WidgetTester tester) async {
+  //     await tester.pumpWidget(MyApp());
+
+  //     final inputText = 'Hello there, this is an input.';
+  //     await tester.enterText(find.byKey(Key('your-text-field')), inputText);
+
+  //     await tester.tap(find.byType(FloatingActionButton));
+  //     await tester.pumpAndSettle();
+
+  //     expect(find.byType(TypingPage), findsNothing);
+  //     expect(find.byType(DisplayPage), findsOneWidget);
+  //     expect(find.text(inputText), findsOneWidget);
+
+  //     await tester.tap(find.byType(BackButton));
+  //     await tester.pumpAndSettle();
+
+  //     expect(find.byType(TypingPage), findsOneWidget);
+  //     expect(find.byType(DisplayPage), findsNothing);
+  //     expect(find.text(inputText), findsNothing);
+  //   },
+  // );
 }
